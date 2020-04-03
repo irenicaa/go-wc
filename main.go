67ea36018d6a81998cb391e1fc7dd1eb0b8b1c00
irenicaa <root@irenica.pro>
2020-04-03 04:15:46 +0300

    ...

--- main.go
+++ main.go
@@ -12,6 +12,8 @@ import (
 func main() {
 	lineCount := 0
 	wordCount := 0
+	symbolCount := 0
+
 	reader := bufio.NewReader(os.Stdin)
 	for {
 		line, err := reader.ReadString('\n')
@@ -35,13 +37,15 @@ func main() {
 			} else {
 				wasSpace = false
 			}
+
 		}
 
 		fmt.Println(wordCountInLine)
 
 		lineCount++
 		wordCount += wordCountInLine
+		symbolCount += len(line)
 	}
 
-	fmt.Println(lineCount, wordCount)
+	fmt.Println(lineCount, wordCount, symbolCount)
 }
