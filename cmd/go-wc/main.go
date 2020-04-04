3410e0e9abf6cbde2211d4ba925615face85b220
irenicaa <root@irenica.pro>
2020-04-04 03:32:12 +0300

    ...

--- cmd/go-wc/main.go
+++ cmd/go-wc/main.go
@@ -26,8 +26,6 @@ func main() {
 			log.Fatal(err)
 		}
 
-		fmt.Print(line)
-
 		wordCountInLine := 0
 		wasSpace := true
 		for _, symbol := range line {
@@ -39,10 +37,7 @@ func main() {
 			} else {
 				wasSpace = false
 			}
-
 		}
-
-		fmt.Println(wordCountInLine)
 
 		lineCount++
 		wordCount += wordCountInLine
