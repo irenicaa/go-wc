deec56b743e7a70ced9d436eda1548e9592063cf
irenicaa <root@irenica.pro>
2020-04-12 03:00:43 +0300

    ...

--- wc.go
+++ wc.go
@@ -38,14 +38,11 @@ func CountWords(text string) int {
 	wordCountInLine := 0
 	wasSpace := true
 	for _, symbol := range text {
-		if unicode.IsSpace(symbol) {
-			if !wasSpace {
-				wordCountInLine++
-			}
-			wasSpace = true
-		} else {
-			wasSpace = false
+		isSpace := unicode.IsSpace(symbol)
+		if isSpace && !wasSpace {
+			wordCountInLine++
 		}
+		wasSpace = isSpace
 	}
 
 	return wordCountInLine
