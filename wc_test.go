a88a9de390f0fc2cb2537abe14c652401e8d543c
irenicaa <root@irenica.pro>
2020-04-17 20:49:46 +0300

    ...

--- wc_test.go
+++ wc_test.go
@@ -50,3 +50,19 @@ func TestCountWords_withUnicode(test *testing.T) {
 	count := CountWords("один два три")
 	assert.Equal(test, 3, count)
 }
+
+func TestStats_AnalyzeLine(test *testing.T) {
+	stats := Stats{LineCount: 1, WordCount: 2, ByteCount: 3, RuneCount: 4}
+	stats.AnalyzeLine("one two three")
+
+	wantStats := Stats{LineCount: 2, WordCount: 5, ByteCount: 16, RuneCount: 17}
+	assert.Equal(test, wantStats, stats)
+}
+
+func TestStats_AnalyzeLine_withUnicode(test *testing.T) {
+	stats := Stats{LineCount: 1, WordCount: 2, ByteCount: 3, RuneCount: 4}
+	stats.AnalyzeLine("один два три")
+
+	wantStats := Stats{LineCount: 2, WordCount: 5, ByteCount: 25, RuneCount: 16}
+	assert.Equal(test, wantStats, stats)
+}
