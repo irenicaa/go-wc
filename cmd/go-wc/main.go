6691842eca51d957bba0f9371ecdd8beaa472a3f
irenicaa <root@irenica.pro>
2020-04-24 02:12:34 +0300

    ...

--- cmd/go-wc/main.go
+++ cmd/go-wc/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -9,10 +10,28 @@ import (
 )
 
 func main() {
+	showLines := flag.Bool("l", false, "show line count")
+	showWords := flag.Bool("w", false, "show word count")
+	showBytes := flag.Bool("c", false, "show byte count")
+	showSymbols := flag.Bool("m", false, "show symbol count")
+	flag.Parse()
+
 	stats, err := wc.AnalyzeReader(os.Stdin)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(stats)
+	if *showLines {
+		fmt.Printf(" %6d", stats.LineCount)
+	}
+	if *showWords {
+		fmt.Printf(" %6d", stats.WordCount)
+	}
+	if *showBytes {
+		fmt.Printf(" %6d", stats.ByteCount)
+	}
+	if *showSymbols {
+		fmt.Printf(" %6d", stats.RuneCount)
+	}
+	fmt.Println()
 }
